ad9774c15262d1c8623360ad64018f08f1cbd8fd
fish895623 <dan990429+github@gmail.com>
2021-09-10 14:49:51 +0900

    고침

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,20 +1,22 @@
 return require('packer').startup(function()
-    use 'https://github.com/wbthomason/packer.nvim'
-    use 'https://github.com/vim-airline/vim-airline'
-    use 'https://github.com/scrooloose/nerdtree'
-    use 'https://github.com/junegunn/vim-easy-align'
-    use 'https://github.com/vim-airline/vim-airline-themes'
-    use 'https://github.com/vim-syntastic/syntastic'
-    use 'https://github.com/vim-scripts/batch.vim'
-    use 'https://github.com/farmergreg/vim-lastplace'
-    use 'https://github.com/godlygeek/tabular'
-    use 'https://github.com/plasticboy/vim-markdown'
-    use 'https://github.com/nvim-lua/plenary.nvim'
-    use 'https://github.com/folke/todo-comments.nvim'
-    use 'https://github.com/custom-vim-ext/render-space'
-    use 'https://github.com/custom-vim-ext/render-number'
-    use 'https://github.com/neovim/nvim-lspconfig'
-    use 'https://github.com/sumneko/lua-language-server'
-    use 'https://github.com/hrsh7th/nvim-compe'
-    use 'https://github.com/liuchengxu/vim-which-key'
+    use {'https://github.com/wbthomason/packer.nvim'}
+    use {'https://github.com/vim-airline/vim-airline'}
+    use {'https://github.com/scrooloose/nerdtree'}
+    use {'https://github.com/junegunn/vim-easy-align'}
+    use {'https://github.com/vim-airline/vim-airline-themes',
+        requires = {'https://github.com/vim-airline/vim-airline'}}
+    use {'https://github.com/vim-syntastic/syntastic'}
+    use {'https://github.com/vim-scripts/batch.vim'}
+    use {'https://github.com/farmergreg/vim-lastplace'}
+    use {'https://github.com/plasticboy/vim-markdown',
+        requires = { 'https://github.com/godlygeek/tabular'}}
+    use {'https://github.com/nvim-lua/plenary.nvim'}
+    use {'https://github.com/folke/todo-comments.nvim'}
+    use {'https://github.com/custom-vim-ext/render-space'}  -- custom plugin
+    use {'https://github.com/custom-vim-ext/render-number'} -- custom plugin
+    use {'https://github.com/neovim/nvim-lspconfig'}        -- language server
+    use {'https://github.com/sumneko/lua-language-server'}  -- lua language
+    use {'https://github.com/ycm-core/YouCompleteMe'}       -- auto complete
+    use {'https://github.com/hrsh7th/nvim-compe'}
+    use {'https://github.com/liuchengxu/vim-which-key'}     -- show which key easy to forget
 end)
